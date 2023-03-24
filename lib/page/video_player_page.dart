3d2607f09e2c0cdc2ffc15e73dc8a092306a4d6b
duygu2 <duyguorhan017@gmail.com>
2023-03-24 15:58:29 +0300

    video player page added

--- lib/page/video_player_page.dart
+++ lib/page/video_player_page.dart
@@ -6,17 +6,48 @@ import 'package:image_picker/image_picker.dart';
 import 'package:video_player/video_player.dart';
 
 class VideoPlayerPage extends StatefulWidget {
-  const VideoPlayerPage({super.key});
+  const VideoPlayerPage({Key? key});
 
   @override
   State<VideoPlayerPage> createState() => _VideoPlayerPageState();
 }
 
 class _VideoPlayerPageState extends State<VideoPlayerPage> {
+  late VideoPlayerController _controller;
+  late Future<void> _video;
+
+  @override
+  void initState() {
+    super.initState();
+    _controller = VideoPlayerController.network(
+        "https://sample-videos.com/video123/mp4/480/big_buck_bunny_480p_1mb.mp4");
+    _video = _controller.initialize();
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Container(
-      child: Text("this page will be refreshed"),
+    return Scaffold(
+      body: FutureBuilder(
+        future: _video,
+        builder: (context, snapshot) {
+          if (snapshot.connectionState == ConnectionState.done) {
+            return AspectRatio(
+              aspectRatio: _controller.value.aspectRatio,
+              child: VideoPlayer(_controller),
+            );
+          } else {
+            return Center(
+              child: CircularProgressIndicator(),
+            );
+          }
+        },
+      ),
     );
+  }
+
+  @override
+  void dispose() {
+    super.dispose();
+    _controller.dispose();
   }
 }
